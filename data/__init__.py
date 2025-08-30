4c14d1e7d354a12a9796f794e3103e27b7364129
Jose Castillo <jcastillo@redhat.com>
2025-08-30 21:22:10 +0100

    [data] Add code to the extract_archive function
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

--- data/__init__.py
+++ data/__init__.py
@@ -3,14 +3,21 @@ This class will deal with different formats of data
 i.e. a sos report, and will uncompress it and review 
 the list of files for the cleaner
 """
+import tarfile
+from pathlib import Path
+
 
 class Data():
     def __init__(self, filename=None, nthreads=4):
         self.filename = filename
         self.ntreads = nthreads
+        self.tmpfilename = "/var/tmp/soscleanAI_dir"
 
     def extract_archive(self):
-        pass
+        Path(self.tmpfilename).mkdir(exist_ok=True)
+        with tarfile.open(self.filename) as arc:
+            # TODO: Change the location of the file extracted
+            arc.extractall(self.tmpfilename)
 
     def read_manifest(self):
         pass
