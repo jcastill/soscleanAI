bfd0e3e397b81a9e60b479f183ab33550b64f4cc
Jose Castillo <jcastillo@redhat.com>
2025-09-08 08:50:47 +0100

    [data] Add logic to distribute files by thread and start execute
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

--- data/__init__.py
+++ data/__init__.py
@@ -15,6 +15,9 @@ class Data():
         self.manifest_location = (f"{self.tmpfilename}"
                                   "/sos_reports/manifest.json")
         self.manifest = []
+        # We'll have a list containing lists of files 
+        # one per defined thread, to go through and obfuscate
+        self.distributed_manifest = []
 
     def extract_archive(self):
         Path(self.tmpfilename).mkdir(exist_ok=True)
@@ -32,4 +35,20 @@ class Data():
                         self.manifest.append(line.split(": ")[1])
 
     def distribute_files_per_thread(self):
-        pass
+        numfiles = len(self.manifest)
+        step = int(numfiles / self.ntreads)
+        # Each thread will go from its index to
+        # 'step'. So if we have 100 files and 4 threads
+        # thread1: 1-25
+        # thread2: 26-50
+        # thread3: 51-75
+        # thread4: 76-100
+
+        for i in range (0, numfiles, step):
+            start_range = i
+            self.distributed_manifest[i] = self.manifest[start_range:step]
+
+    def execute(self):
+        self.extract_archive()
+        self.read_manifest()
+        self.distribute_files_per_thread()
